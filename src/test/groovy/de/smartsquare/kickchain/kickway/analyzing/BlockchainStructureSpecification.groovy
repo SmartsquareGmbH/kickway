9231efab14e172c6910b893f672d390f8b212ed7
Ruben Gees <rubengees7@googlemail.com>
2018-08-18 19:39:47 +0200

    Fix blockchain parsing test

--- src/test/groovy/de/smartsquare/kickchain/kickway/analyzing/BlockchainStructureSpecification.groovy
+++ src/test/groovy/de/smartsquare/kickchain/kickway/analyzing/BlockchainStructureSpecification.groovy
@@ -5,46 +5,33 @@ import org.springframework.web.client.RestTemplate
 import spock.lang.Shared
 import spock.lang.Specification
 
-import java.nio.file.Files
-import java.nio.file.Path
-
-import static org.mockserver.integration.ClientAndServer.startClientAndServer
 import static org.mockserver.matchers.Times.exactly
 import static org.mockserver.model.HttpRequest.request
 import static org.mockserver.model.HttpResponse.response
 
 class BlockchainStructureSpecification extends Specification {
 
-    def kickchain = new RestTemplate()
+    def client = new RestTemplate()
 
     @Shared
-    ClientAndServer clientAndServer = startClientAndServer(8111)
+    ClientAndServer server = new ClientAndServer()
 
     def setupSpec() {
-        clientAndServer.when(request()
+        server.when(request()
                 .withMethod("GET")
                 .withPath("/chain"), exactly(1))
                 .respond(response()
                 .withStatusCode(200)
                 .withHeader("Content-Type", "application/json")
-                .withBody(readBlockchainJSONFromTestResources()))
+                .withBody(this.getClass().classLoader.getResource("blockchain.json").text))
     }
 
-    def readBlockchainJSONFromTestResources() {
-        URL blockchainURL = this.getClass().getResource("/blockchain.json")
-        URI blockchainURI = blockchainURL.toURI()
-        Path blockchainPath = new File(blockchainURI).toPath()
-        return Files.readAllBytes(blockchainPath)
+    def cleanupSpec() {
+        server.stop()
     }
 
     def 'parse blockchain from json'() {
         expect:
-        kickchain.getForObject("http://localhost:8111/chain", Blockchain.class).blocks.size() == 10
+        client.getForObject("http://localhost:${server.port}/chain", Blockchain.class).blocks.size() == 10
     }
-
-
-    def cleanupSpec() {
-        clientAndServer.stop()
-    }
-
 }
