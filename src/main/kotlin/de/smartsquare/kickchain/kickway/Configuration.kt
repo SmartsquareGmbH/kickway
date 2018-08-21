0a89c1d4c961527669ea8f8cbacf127510b95f95
Dennis Dierkes <dierkes@smartsquare.de>
2018-08-21 10:11:51 +0200

    Rename configuration class

--- src/main/kotlin/de/smartsquare/kickchain/kickway/Configuration.kt
+++ src/main/kotlin/de/smartsquare/kickchain/kickway/Configuration.kt
@@ -1,15 +1,18 @@
 package de.smartsquare.kickchain.kickway
 
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.web.client.RestTemplate
 
 @Configuration
-class HttpBasicAuthConfiguration(private val properties: KickwayConfigurationProperties) {
+class Configuration(private val properties: KickwayConfigurationProperties) {
 
     @Bean
     fun restTemplateWithBasicAuth(restTemplateBuilder: RestTemplateBuilder): RestTemplate {
-        return restTemplateBuilder.basicAuthorization(properties.kickchain.authorization.name, properties.kickchain.authorization.password).build()
+        return restTemplateBuilder.basicAuthorization(
+            properties.kickchain.authorization.name,
+            properties.kickchain.authorization.password
+        ).build()
     }
 }
