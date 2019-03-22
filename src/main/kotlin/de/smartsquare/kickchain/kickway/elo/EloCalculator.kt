0790aae6db92639358fe16bcd673565649ae2ff3
Dennis Dierkes <dierkes@smartsquare.de>
2019-03-22 13:11:35 +0100

    Improve kdoc

--- src/main/kotlin/de/smartsquare/kickchain/kickway/elo/EloCalculator.kt
+++ src/main/kotlin/de/smartsquare/kickchain/kickway/elo/EloCalculator.kt
@@ -3,7 +3,10 @@ package de.smartsquare.kickchain.kickway.elo
 import de.smartsquare.kickchain.kickway.Blockchain
 
 /**
- * @see https://de.wikipedia.org/wiki/Elo-Zahl (Anpassung der Elo-Zahl)
+ * This method is used to recalculate the elo scores after a match has been played.
+ *
+ * @return a copy of the both EloRating with an adjusted elo score.
+ * @see https://en.wikipedia.org/wiki/Elo_rating_system
  */
 fun Blockchain.Block.Game.readjust(one: EloRating, other: EloRating): AdjustedEloScore {
     val newEloOne =
@@ -17,7 +20,10 @@ fun Blockchain.Block.Game.readjust(one: EloRating, other: EloRating): AdjustedEl
 }
 
 /**
- * @see https://de.wikipedia.org/wiki/Elo-Zahl (Anpassung der Elo-Zahl)
+ * The match factor is part of the elo formula.
+ *
+ * @return one in case of a victory or zero in case of a defeat.
+ * @see https://en.wikipedia.org/wiki/Elo_rating_system
  */
 fun Blockchain.Block.Game.matchFactor(firstPlayer: String, secondPlayer: String) =
     if (this.team1.players.containsAll(listOf(firstPlayer, secondPlayer)) && this.score.goals1 == 10) {
@@ -29,7 +35,10 @@ fun Blockchain.Block.Game.matchFactor(firstPlayer: String, secondPlayer: String)
     }
 
 /**
- * @see https://de.wikipedia.org/wiki/Elo-Zahl (Anpassung der Elo-Zahl)
+ * The experience factor is part of the elo formula to treat the different player levels.
+ *
+ * @return 10 in case of a top player, 20 by default and 40 in case of a newbie.
+ * @see https://en.wikipedia.org/wiki/Elo_rating_system
  */
 fun EloRating.experienceFactor() = when {
     this.elo > 2400 -> 10
@@ -38,7 +47,10 @@ fun EloRating.experienceFactor() = when {
 }
 
 /**
- * @see https://de.wikipedia.org/wiki/Elo-Zahl (Erwartungswert)
+ * The odds is part of the elo formula and is used to treat the skill gap.
+ *
+ * @return the probability for the instance to defeat the opposite.
+ * @see https://en.wikipedia.org/wiki/Elo_rating_system
  */
 infix fun EloRating.odds(opposite: EloRating) =
     (1 / (1 + Math.pow(10.0, (opposite.elo - this.elo) / 400))).roundToThreeDecimals()
